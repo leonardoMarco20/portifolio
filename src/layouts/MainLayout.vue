7ea4c46bc9bbe618a668e3eea4452c3d8fc533aa
Leo <leonardo.silva.marco@gmail.com>
2023-05-07 23:24:51 -0300

    perfil section

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,38 +1,9 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-        </q-item-label>
-
-        
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -94,12 +65,12 @@ export default defineComponent({
   components: {
   },
 
-  setup () {
+  setup() {
     const leftDrawerOpen = ref(false)
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
